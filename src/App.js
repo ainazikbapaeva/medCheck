d901b1510563507932bc1754d9f0712b7f517bb0
tulobaev <talgattulobaev519>
2024-01-25 01:46:50 +0600

    article task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import styled from 'styled-components';
+import "./App.css";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
-      
+      <Main />
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Article from "./components/Article/Article";
 
 const Main = () => {
   return (
     <div>
-      
+      <Article />
     </div>
   );
-}
+};
 
 export default Main;
