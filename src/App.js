7102c19b033bbd8ea885adcc31afc01925d6f7b6
tulobaev <talgattulobaev519>
2024-01-25 01:49:39 +0600

    changes file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import Main from "./Main";
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
