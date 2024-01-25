043daaf22ed6697a0e314332c07d77bff3d13196
Ainazik <ajnazikbapaeva@gmail.com>
2024-01-25 15:28:51 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 import styled from "styled-components";
-import Main from "./Main";
-
 function App() {
   return (
     <div className="App">
-      <Main />
     </div>
   );
 }
